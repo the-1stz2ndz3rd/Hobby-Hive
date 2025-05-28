b01bff8f536ff8cb4581ab6ccff92cfde68441b7
mamme-abdu <Mohammedabdu5516@gmail.com>
2025-05-28 19:21:33 +0300

    Add files via upload

---
+++ notifications.py
@@ -0,0 +1,8 @@
+# notifications.py
+import tkinter.messagebox as messagebox
+
+def trigger_notification(type, message):
+    messagebox.showinfo(type, message)
+
+def display_notifications():
+    pass
