1de38992f4b2f149522b11f7c6b7f68e39b040e3
Mekdes Manaye <olithe1stthe2ndthe3rd@gmail.com>
2025-05-31 12:16:14 +0300

    Updated File Structures

--- notifications.py
+++
@@ -1,8 +0,0 @@
-# notifications.py
-import tkinter.messagebox as messagebox
-
-def trigger_notification(type, message):
-    messagebox.showinfo(type, message)
-
-def display_notifications():
-    pass
