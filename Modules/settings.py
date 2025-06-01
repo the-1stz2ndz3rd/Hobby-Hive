a8efa966548b2525edd15c14ac681a80e6857986
Feven Gebremedhin <femousgeb@gmail.com>
2025-06-01 13:21:50 +0300

    Update settings.py

--- Modules/settings.py
+++ Modules/settings.py
@@ -1,16 +1,23 @@
 # modules/settings.py
 from utils.data_storage import load_json, save_json
 
-SETTINGS_FILE = "data/settings.json"
+USERS_FILE = "data/users_data.json"
 
-def get_settings():
-    settings = load_json(SETTINGS_FILE)
-    if not settings:
-        settings = {"dark_mode": False}
+def get_user_settings(username):
+    users = load_json(USERS_FILE)
+    user = users.get(username, {})
+    settings = user.get("settings", {"dark_mode": False})
     return settings
 
-def toggle_dark_mode():
-    settings = get_settings()
+def toggle_dark_mode(username):
+    users = load_json(USERS_FILE)
+    user = users.get(username)
+    if not user:
+        return False  # User not found
+
+    settings = user.get("settings", {"dark_mode": False})
     settings["dark_mode"] = not settings.get("dark_mode", False)
-    save_json(SETTINGS_FILE, settings)
+    user["settings"] = settings
+    users[username] = user
+    save_json(USERS_FILE, users)
     return settings["dark_mode"]
